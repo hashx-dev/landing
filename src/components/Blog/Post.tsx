62569baa0a82af0e54869cce4ee8d271cfa384e8
shivam2003sy <shivam2003sy@gmail.com>
2023-02-01 14:45:59 +0530

    button clickable

--- src/components/Blog/Post.tsx
+++ src/components/Blog/Post.tsx
@@ -3,10 +3,12 @@ import Card from '@mui/material/Card';
 import { SvgIcon } from "../../common/SvgIcon";
 import {Button} from "../../common/Button"
 import {Link} from "react-router-dom"
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import {
     Content ,
     ButtonWrapper
 } from "./style"
+import ButtonLink from '../../common/ButtonLink';
 type Props = {
     id: Number,
     title : string,
@@ -14,26 +16,34 @@ type Props = {
     content : string
     filename: string
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      minWidth: '300px',
+      minHeight: '300px',
+    },
+    button: {
+      position: 'absolute',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  })
+);
 export const Post = (props: Props) => {
+  const classes = useStyles(); 
   return (
-    <Card sx={{ maxWidth: 325  , m:2 , p:2}} >
+    <Card sx={{ maxWidth: 325  , m:2 , p:2}} className={classes.card} >
     <title>{props.title}</title>
       <SvgIcon src={props.src} width="auto" height="auto" />
       <Content>
         {props.content}
     </Content>
       <ButtonWrapper>
-        <Button 
-        key={1}
-        color="white"
-        fixedWidth={true}
-        onClick={() => {
-            return console.log(props.filename)
-        }}
-        >
-        <Link to={{pathname: `/blog/${props.filename}`}}>
-        Read Blog </Link>
-        </Button>
+          <ButtonLink to={`/blog/${props.filename}`}  text={"Read More"} className={classes.button}/>
       </ButtonWrapper>
     </Card>
   )
