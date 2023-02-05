954d254b019db80554679dd98c5b015c6d27a5b5
Rajarshi Prattipati <rishiprattipati@gmail.com>
2023-02-05 16:24:57 +0530

    save

--- src/components/Blog/Post.tsx
+++ src/components/Blog/Post.tsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import { SvgIcon } from "../../common/SvgIcon";
 // import {Button} from "../../common/Button"
 // import {Link} from "react-router-dom"
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import {
     Content ,
     ButtonWrapper
@@ -17,39 +17,39 @@ type Props = {
     filename: string
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    card: {
-      position: 'relative',
-      display: 'flex',
-      flexDirection: 'column',
-      minWidth: '300px',
-      minHeight: '300px',
-    },
-    button: {
-      position: 'absolute',
-      bottom: theme.spacing(2),
-      right: theme.spacing(1),
-      left : theme.spacing(5),
-      width : "100%"
-    },
-    div :{
-      margin : '20px'
-    }
-  })
-);
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     card: {
+//       position: 'relative',
+//       display: 'flex',
+//       flexDirection: 'column',
+//       minWidth: '300px',
+//       minHeight: '300px',
+//     },
+//     button: {
+//       position: 'absolute',
+//       bottom: theme.spacing(2),
+//       right: theme.spacing(1),
+//       left : theme.spacing(5),
+//       width : "100%"
+//     },
+//     div :{
+//       margin : '20px'
+//     }
+//   })
+// );
 export const Post = (props: Props) => {
-  const classes = useStyles(); 
+  // const classes = useStyles(); 
   return (
-    <Card sx={{ maxWidth: 325  , m:2 , p:2}} className={classes.card} >
+    <Card sx={{ maxWidth: 325  , m:2 , p:2}} >
     <title>{props.title}</title>
       <SvgIcon src={props.src} width="auto" height="auto" />
       <Content>
         {props.content}
     </Content>
-    <div className={classes.div}>
-      <ButtonWrapper className={classes.button}>
-          <ButtonLink to={`/blog/${props.filename}`}  text={"Read More"} className={classes.button}/>
+    <div >
+      <ButtonWrapper >
+          <ButtonLink to={`/blog/${props.filename}`}  text={"Read More"} />
       </ButtonWrapper>
       </div>
     </Card>
